6d84cf51fc1d26d3810115580b2172c8e8ffc0d6
Iker <ikermartiinsv@gmail.com>
2026-03-29 11:49:02 +0000

    chore(auto): session 73f5c871 2026-03-29

--- database/migrations/20260316_sessions_compact_count.sql
+++
@@ -1,4 +0,0 @@
--- Migration: add compact_count to sessions
--- Tracks how many PreCompact events occurred in each session.
-
-ALTER TABLE sessions ADD COLUMN compact_count INTEGER DEFAULT 0;
--- database/migrations/20260325_knowledge_usage_benchmark.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE knowledge_usage ADD COLUMN query TEXT;
-ALTER TABLE knowledge_usage ADD COLUMN feedback TEXT;
